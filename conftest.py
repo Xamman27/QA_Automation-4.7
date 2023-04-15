5b885d53ea6821f76d8a89d60f5a8de24de70e25
Xamman27 <xamman27@gmail.com>
2023-04-15 23:48:36 +0400

    поиск расширения файла

--- conftest.py
+++ conftest.py
@@ -1,1 +1,13 @@
+import os
+import zipfile
 
+
+path_zip = os.path.join(os.path.abspath('resources'), 'zip_for_test')
+with zipfile.ZipFile(path_zip, 'r') as archive:
+    for file in archive.namelist():
+        if file.endswith('pdf'):
+            print(file)
+        elif file.endswith('csv'):
+            print(file)
+        elif file.endswith('xls'):
+            print(file)
--- zip_file.py
+++ zip_file.py
@@ -1,20 +1,27 @@
 import os
 import zipfile
+import pytest
 import csv
 
 
-def mk_zip_archive(path_to_zip): #создаем архив в папке resources
+def test_mk_zip_archive(): #создаем архив в папке resources
+    path_files_for_test = os.path.abspath('files_for_test')
     with zipfile.ZipFile(os.path.join(os.path.abspath('resources'), 'zip_for_test'), 'w') as zf:
-        file_to_zip = os.listdir(path_to_zip) #список файлов в архив из папки files_for_test
+        file_to_zip = os.listdir(path_files_for_test) #список файлов в архив из папки files_for_test
         for file in file_to_zip:
-            add_file = os.path.join(path_to_zip, file)
+            add_file = os.path.join(path_files_for_test, file)
             zf.write(add_file)
+        assert str(zf).endswith('zip_for_test')
 
-#
-#
-# with zipfile.ZipFile(path_zip, 'r') as zfolder:
-#      for i in zfolder.namelist():
-#          print(i)
-path_files_for_test = os.path.abspath('files_for_test')
-mk_zip_archive(path_files_for_test)#функция архивирования файлов
-path_zip = os.path.join(os.path.abspath('resources'), 'zip_for_test')
+def test_csv():
+    with zipfile.ZipFile(os.path.join(os.path.abspath('resources'), 'zip_for_test'), 'r') as archive:
+        for file in archive.namelist():
+            if file.endswith('csv'):
+                print(file)
+
+
+
+
+
+
+
