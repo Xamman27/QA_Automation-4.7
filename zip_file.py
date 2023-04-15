a5574c64638a460080645d4824d07f437f104e16
Xamman27 <xamman27@gmail.com>
2023-04-15 12:24:17 +0400

    обернул созданине архива в функцию

--- zip_file.py
+++ zip_file.py
@@ -2,16 +2,19 @@ import os
 import zipfile
 import csv
 
-path_to_zip = os.path.abspath('files_for_test')
-file_to_zip = os.listdir(path_to_zip)
 
-with zipfile.ZipFile(os.path.join(os.path.abspath('resources'), 'zip_for_test'), 'w') as zf:
-    for file in file_to_zip:
-        add_file = os.path.join(path_to_zip, file)
-        zf.write(add_file)
+def mk_zip_archive(path_to_zip): #создаем архив в папке resources
+    with zipfile.ZipFile(os.path.join(os.path.abspath('resources'), 'zip_for_test'), 'w') as zf:
+        file_to_zip = os.listdir(path_to_zip) #список файлов в архив из папки files_for_test
+        for file in file_to_zip:
+            add_file = os.path.join(path_to_zip, file)
+            zf.write(add_file)
 
+#
+#
+# with zipfile.ZipFile(path_zip, 'r') as zfolder:
+#      for i in zfolder.namelist():
+#          print(i)
+path_files_for_test = os.path.abspath('files_for_test')
+mk_zip_archive(path_files_for_test)#функция архивирования файлов
 path_zip = os.path.join(os.path.abspath('resources'), 'zip_for_test')
-with zipfile.ZipFile(path_zip, 'r') as zf:
-     zfile = csv.reader(os.path.join(path_zip, 'csv_example'))
-     for r in zfile:
-        print(r)
